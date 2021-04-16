9adca73e18343fc4a1984831ef02727c6ee3dffb
PierreYvesO <oupindrin.pierrey@outlook.com>
2021-04-16 15:25:33 +0200

    implement getFacesWithSameVertex

--- New Unity Project/Assets/GeometricModeling.cs
+++ New Unity Project/Assets/GeometricModeling.cs
@@ -241,6 +241,20 @@ namespace HalfEdge
             return adjacentEdges;
         }
 
+
+        public List<Face> getFacesWithSameVertex(Vertex vertex)
+        {
+            List<Face> faces = new List<Face>();
+            foreach(HalfEdge edge in getAdjacentEdges(vertex))
+            {
+                if(!faces.Contains(edge.face))
+                {
+                    faces.Add(edge.face);
+                }
+            }
+            return faces;
+        }
+
         public override string ToString()
         {
             string str = "Vertices\t\t\t\tFaces\t\t\tHalfEdges\n";
@@ -287,6 +301,8 @@ public class GeometricModeling : MonoBehaviour
         Debug.Log(exportMeshToExcel(mesh));
         List<HalfEdge.HalfEdge> adjacentEdgesOfVertex = halfEdgeMesh.getAdjacentEdges(halfEdgeMesh.vertices[2]);
         Debug.Log(string.Join("\n", adjacentEdgesOfVertex));
+        Debug.Log(string.Join("\n", halfEdgeMesh.getFacesWithSameVertex(halfEdgeMesh.vertices[2])));
+
     }
 
     string exportMeshToExcel(Mesh mesh)
